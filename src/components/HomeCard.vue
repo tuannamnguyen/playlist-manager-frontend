c3402371ac0806d66a3ac09c33f57e396b8a6009
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-10-07 23:48:24 +0700

    update playlist aspect ratio

--- src/components/HomeCard.vue
+++ src/components/HomeCard.vue
@@ -12,7 +12,9 @@ const { image, title, subTitle, user } = toRefs(props)
 
 <template>
     <div class="bg-[#111111] p-4 rounded-md hover:bg-[#252525] cursor-pointer m-2">
-        <img class="rounded-md w-full h-48 object-cover" :src="image" :alt="title">
+        <div class="aspect-square w-full overflow-hidden rounded-md">
+            <img class="w-full h-full object-cover" :src="image" :alt="title">
+        </div>
         <div class="text-white pt-4 font-semibold text-[17px]">{{ title }}</div>
         <div class="text-gray-400 pt-1 text-[14px]">{{ subTitle }}</div>
         <div class="text-gray-400 pt-1 pb-3 text-[14px]">Created by {{ user }}</div>
