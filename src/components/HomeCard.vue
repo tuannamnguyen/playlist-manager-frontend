7eb03899e4026a3ce17b4e016c2791079d7ad34c
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-10-08 21:33:13 +0700

    update all card same size

--- src/components/HomeCard.vue
+++ src/components/HomeCard.vue
@@ -9,14 +9,17 @@ const props = defineProps({
 })
 const { image, title, subTitle, user } = toRefs(props)
 </script>
-
 <template>
-    <div class="bg-[#111111] p-4 rounded-md hover:bg-[#252525] cursor-pointer m-2">
-        <div class="w-40 h-40 mx-auto overflow-hidden rounded-md">
+    <div class="bg-[#111111] p-4 rounded-md hover:bg-[#252525] cursor-pointer m-2 w-64 h-80 flex flex-col">
+        <div class="w-56 h-56 mx-auto overflow-hidden rounded-md flex-shrink-0">
             <img class="w-full h-full object-cover" :src="image" :alt="title">
         </div>
-        <div class="text-white pt-4 font-semibold text-[17px]">{{ title }}</div>
-        <div class="text-gray-400 pt-1 text-[14px]">{{ subTitle }}</div>
-        <div class="text-gray-400 pt-1 pb-3 text-[14px]">Created by {{ user }}</div>
+        <div class="flex flex-col justify-between flex-grow mt-2">
+            <div>
+                <div class="text-white font-semibold text-[17px] truncate">{{ title }}</div>
+                <div class="text-gray-400 text-[14px] truncate">{{ subTitle }}</div>
+            </div>
+            <div class="text-gray-400 text-[14px] truncate">Created by {{ user }}</div>
+        </div>
     </div>
 </template>
