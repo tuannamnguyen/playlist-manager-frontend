59359291f2fa7d710f9f6cb4c0b2004a7a3b631d
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-12-02 11:20:34 +0700

    update files

--- terraform/prod/cloud_storage.tf
+++ terraform/prod/cloud_storage.tf
@@ -1,6 +1,35 @@
-resource "google_storage_bucket_object" "indexpage" {
-  bucket       = module.static-assets_cloud-storage-static-website.website_bucket_name
-  name         = "index.html"
-  content_type = "text/html"
-  source       = "${path.root}/../../dist/index.html"
+locals {
+  dist_directory = "${path.root}/../../dist/"
+
+  mime_types = {
+    htm  = "text/html"
+    html = "text/html"
+    css  = "text/css"
+    ttf  = "font/ttf"
+    js   = "application/javascript"
+    map  = "application/javascript"
+    json = "application/json"
+    jpg  = "image/jpeg"
+    ico  = "image/x-icon"
+    png  = "image/png"
+  }
+}
+
+data "local_file" "files" {
+  for_each = fileset(local.dist_directory, "**")
+  filename = "${local.dist_directory}${each.key}"
+}
+
+
+resource "google_storage_bucket_object" "static_files" {
+  for_each = data.local_file.files
+
+  bucket = module.static-assets_cloud-storage-static-website.website_bucket_name
+  name   = each.key
+  source = each.value.filename
+  content_type = lookup(
+    local.mime_types,
+    regex("[^.]+$", each.key),
+    "application/octet-stream"
+  )
 }
